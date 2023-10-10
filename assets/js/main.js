e8349be85d3db8b58d130c31c2d11fe0acbcaeb7
hieunguyen810 <trihieu08101999@gmail.com>
2023-10-10 21:36:54 +0700

    update readme

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,14 +113,14 @@ function generateResume(){
 // When the button is clicked, it executes the three functions
 resumeButton.addEventListener('click', () => {
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-    scaleCv()
+    // scaleCv()
 
     // 2. The PDF is generated
-    generateResume()
+    // generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
-    setTimeout(removeScale, 5000)
-//     window.open('/assets/pdf/NGUYENTRIHIEU_Resume.pdf')
+    // setTimeout(removeScale, 5000)
+    window.open('/assets/pdf/NGUYENTRIHIEU_Resume.pdf')
 })
 
 // Change language
